82941a75c9eb120eaa1936469f402bb82658657d
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-05-12 14:36:41 -0400

    Switch Robots To Jsonplaceholder Users

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import CardList from "./CardList"
 import SearchBox from "./SearchBox"
-import { robots } from "./robots"
 import './App.css'
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
+        // this.setState( {robots: robots });
     }
 
     onSearchChange = (event) => {
@@ -21,6 +27,9 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
+        if (this.state.robots.length === 0) {
+            return <h1> Loading... </h1>
+        }
         return (
             <div className='tc'>
                 <h1 className='f2'>RoboFriends</h1>
